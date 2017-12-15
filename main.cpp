df7d393eb4cc481ffe4e48ca97249277d1c6d91a
holu123gg <33393641+holu123gg@users.noreply.github.com>
2017-12-15 20:50:31 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string a;
+    cout<<"input  the  path of music: "<<endl;
+    cin>>a;
+    
+    cout<<"confirm : "<<endl<<a<<endl;
+    
+    //check whether  path is right
+    //check whether  music is already
+    //cut by  other users
+    //if  "yes"  then  download  the 
+    // timeline and  cut  the  music
+    //if "no"  then  cut  the music
+    //,upload  timeline
+    //caution:there may be many music
+    
+    
+
+    cout<<"done"
+    return 0;
+}
